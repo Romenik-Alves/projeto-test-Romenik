46788ffd053a33f6196b116fe3d44716e2fee86e
Romenik <romenikga@gmail.com>
2024-07-16 19:56:56 -0400

    Versao 1

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print('Olá, estamos no nosso primeiro projeto!')
+#print('Olá, estamos no nosso primeiro projeto!')
+numero = 1
+while numero <= 10:
+    print(numero)
+    if numero == 5:
+        break
+    numero += 1
