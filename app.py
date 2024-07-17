0973925ee0d8ca0fb190538c53588b229c44237f
Romenik <romenikga@gmail.com>
2024-07-16 20:10:22 -0400

    adicionados aquivo de tuplas

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-#print('Olá, estamos no nosso primeiro projeto!')
-numero = 1
-while numero <= 10:
-    print(numero)
-    if numero == 5:
-        break
-    numero += 1
+
+print('Olá, estamos no nosso primeiro projeto!')
+
---
+++ tuplas.py
@@ -0,0 +1,6 @@
+numero = 1
+while numero <= 10:
+    print(numero)
+    if numero == 5:
+        break
+    numero += 1
