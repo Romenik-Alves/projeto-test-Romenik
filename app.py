321112f9f87a052832918c6757168cee737493e0
Romenik <romenikga@gmail.com>
2024-08-18 08:26:46 -0400

    Versao 1

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+print('Olá')
 
-print('Olá, estamos no nosso primeiro projeto!')
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
 
+
+calcular_preco_combo(30, 20)
+print('programa finalizado')
